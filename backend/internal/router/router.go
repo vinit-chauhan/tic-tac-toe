67a183deb773b3dc3413e453d78541c272476e07
vinit-chauhan <chauhanvinit23@gmail.com>
2024-11-01 15:28:16 -0400

    backend: fix CORS error

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -2,12 +2,29 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+
 	"github.com/vinit-chauhan/tic-tac-toe/internal/controllers/auth"
 	"github.com/vinit-chauhan/tic-tac-toe/internal/controllers/user"
 	"github.com/vinit-chauhan/tic-tac-toe/internal/middleware"
+	"github.com/vinit-chauhan/tic-tac-toe/utils/logger"
 )
 
 func SetRoutes(r *gin.Engine) {
+
+	logger.Info("setting up CORS middleware", "main")
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
 
 	// Auth endpoints
 	r.POST("/auth/login", auth.Login)
